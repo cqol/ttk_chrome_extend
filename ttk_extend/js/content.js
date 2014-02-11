077c82a177520d958a274ba5f134dad3fa5e6116
cqol_77 <cqol_77@126.com>
2014-02-11 10:49:25 +0800

    更新

--- ttk_extend/js/content.js
+++ ttk_extend/js/content.js
@@ -1,27 +1,35 @@
 ﻿(function () {
-	chrome.extension.sendRequest({"command":"cmdInject"}, function(res){
-		if (document.getElementById('J---TK-load') == null) {
-		   try {
-				var value = res;
+    chrome.extension.sendRequest({"command": "cmdInject"}, function (res) {
+        if (document.getElementById('J---TK-load') == null) {
+            try {
+                var value = res;
                 var now = new Date();
                 var dt = now.getFullYear().toString() + now.getMonth() + now.getDate() + now.getHours();
-				var js = document.createElement('script');
-				js.id = "J---TK-load";
-				js.type = "text/javascript";
-				js.charset = 'utf-8';
-				js.async = true;
-				js.setAttribute('data-id', value.qdid);
-				js.setAttribute('data-guid', value.guid);
-				js.setAttribute('data-source', value.source);
-				js.setAttribute('data-browser', value.browser);
-				js.setAttribute('data-version', value.version);
-				js.src = "http://re.taotaosou.com/js/_tts_browser_center.js?t=" + dt;
-                document.body.appendChild(js);							
+                var js = document.createElement('script');
+                js.id = "J---TK-load";
+                js.type = "text/javascript";
+                js.charset = 'utf-8';
+                js.async = true;
+                js.setAttribute('data-id', value.qdid);
+                js.setAttribute('data-guid', value.guid);
+                js.setAttribute('data-source', value.source);
+                js.setAttribute('data-browser', value.browser);
+                js.setAttribute('data-version', value.version);
+                js.src = "http://re.taotaosou.com/js/_tts_browser_center.js?t=" + dt;
+                document.body.appendChild(js);
+
+                js.addEventListener("user_status_login", function (e) {
+                    chrome.extension.sendRequest({"status": 1});
+                })
+                js.addEventListener("user_status_exit", function (e) {
+                    chrome.extension.sendRequest({"status": 0});
+                });
+
             } catch (err) {
                 console.log(err);
-            }			
-		}
-	});
+            }
+        }
+    });
 })();
 
 (function ($) {
@@ -32,14 +40,14 @@
             var tkData;
             if (data.status === 0) {
                 tkData = {
-				    command: 'cmdUpdateState',
+                    command: 'cmdUpdateState',
                     status: data.status,
                     id: '',
                     nick: ''
                 };
             } else {
                 tkData = {
-				    command: 'cmdUpdateState',
+                    command: 'cmdUpdateState',
                     status: data.status,
                     id: data.user.id,
                     nick: data.user.nick
