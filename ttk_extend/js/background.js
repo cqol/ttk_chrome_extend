0757cc7b51d616da4976fe6f9c4aa3c3098083a6
chenqi <chenqi@taotaosou.com>
2014-06-16 15:41:48 +0800

    login loginOut status layout

--- ttk_extend/js/background.js
+++ ttk_extend/js/background.js
@@ -14,6 +14,23 @@ taotaosou.extension.config.getGuid = function () {
         return v.toString(16);
     }).toUpperCase();
 };
+//这个函数可以检测到客户端发来的消息
+function clientmsg(msg) {
+    alert(msg)
+}
+
+//这个函数给客户端发消息
+function sentClientData(data) {
+    try {
+        var plugin = document.getElementById("taotaosouplugin");
+        if (plugin) {
+            plugin.sendDatatoClient(data);
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
 // init the config data
 taotaosou.extension.config.init = function () {
@@ -65,65 +82,115 @@ taotaosou.extension.config.init = function () {
     })(taotaosou.extension.config.data);
 };
 
-// init the plugin and read the config data
+// read the config data from plugin
 taotaosou.extension.config.readLocalData = function () {
     try {
-        var plugin = document.createElement("embed");
-        plugin.setAttribute('type', 'application/x-taotaosou-extension');
-        plugin.setAttribute('hidden', true);
-        plugin.setAttribute('id', 'taotaosouplugin');
-        document.body.appendChild(plugin);
-        var tmp = plugin.getQdid();
-        if (tmp != "Unknow") {
-            taotaosou.extension.config.data.qdid = tmp;
+        var plugin = document.getElementById("taotaosouplugin");
+        if (plugin) {
+            var tmp = plugin.getQdid();
+            if (tmp != "Unknow") {
+                taotaosou.extension.config.data.qdid = tmp;
+            }
+            taotaosou.extension.config.data.actived = plugin.isActive(taotaosou.extension.config.data.type);
+            taotaosou.extension.config.data.browser = plugin.getBrowser();
+            tmp = plugin.getGuid();
+            if (tmp != "Unknow") {
+                taotaosou.extension.config.data.guid = tmp;
+            }
         }
-        taotaosou.extension.config.data.actived = plugin.isActive(taotaosou.extension.config.data.type);
-        taotaosou.extension.config.data.browser = plugin.getBrowser();
-        tmp = plugin.getGuid();
-        if (tmp != "Unknow") {
-            taotaosou.extension.config.data.guid = tmp;
-        }
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err);
     }
 };
 
-taotaosou.extension.requestHandler = function( request, sender, sendResponse ){
+taotaosou.extension.config.isNeedMsg = function () {
+
+    var isNeedMsg = 1; // 默认打开
+    try {
+        var plugin = document.getElementById("taotaosouplugin");
+        if (plugin) {
+            var tmp1 = plugin.isNeedMsg();
+            if (tmp1 == false) {
+                isNeedMsg = '0';
+            }
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+    return isNeedMsg;
+}
+
+taotaosou.extension.config.getClientUseId = function () {
+    var clientUserId = "UnLogin"
+    try {
+        var plugin = document.getElementById("taotaosouplugin");
+        if (plugin) {
+            clientUserId = plugin.getClientUserID();
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+    return clientUserId;
+
+}
+
+taotaosou.extension.requestHandler = function (request, sender, sendResponse) {
     if (request.command == "cmdInject") {
-	    sendResponse( JSON.parse(localStorage.tts_config_data) );
-	}
+        var sent = JSON.parse(localStorage.tts_config_data);
+        sent.needmessage = taotaosou.extension.config.isNeedMsg();
+        sent.clientuserid = taotaosou.extension.config.getClientUseId();
+        sendResponse(sent);
+    }
+};
+
+taotaosou.extension.getOSFromUseragent = function () {
+    var retOS = "Unknow";
+    try {
+        var userAgent = window.navigator.userAgent;
+        var matchList = userAgent.match(/([^(]+?)(?=\))/g);
+        if (matchList != null) {
+            retOS = matchList[0];
+        }
+    } catch (err) {
+        console.log(err);
+    }
+    return retOS;
 };
 
 (function () {
     var isActive = localStorage.tts_chrome_isActive;
-    taotaosou.extension.config.init();	
-	
-    if (isActive) {       
-		var value = localStorage.tts_config_data;
-		if ( typeof(value) != "undefined" ){
-			value = JSON.parse( value );
-		}
-		// forward compatible ( <=1.3.9 )
-		if ( typeof(value) == "undefined" || typeof(value.actived) == "undefined" ) {
-			taotaosou.extension.config.readLocalData();
-			localStorage.tts_config_data = JSON.stringify(taotaosou.extension.config.data);
-		// update the version and default browsr when extension upgrade has completed.
-		} else if ( value != null && value.version != null && value.version != taotaosou.extension.config.data.version) {
-			taotaosou.extension.config.readLocalData();
-			value.version = taotaosou.extension.config.data.version;
-			value.browser = taotaosou.extension.config.data.browser;
-			localStorage.tts_config_data = JSON.stringify(value);
-			taotaosou.extension.config.data = value;
-		}
+    taotaosou.extension.config.init();
+
+    if (isActive == "true") {
+        var value = localStorage.tts_config_data;
+        if (typeof(value) != "undefined") {
+            value = JSON.parse(value);
+        }
+        // forward compatible ( <=1.3.9 )
+        if (typeof(value) == "undefined" || typeof(value.actived) == "undefined") {
+            taotaosou.extension.config.readLocalData();
+            localStorage.tts_config_data = JSON.stringify(taotaosou.extension.config.data);
+            // update the version and default browsr when extension upgrade has completed.
+        }
+        else if (value != null && value.version != null && value.version != taotaosou.extension.config.data.version) {
+            taotaosou.extension.config.readLocalData();
+            value.version = taotaosou.extension.config.data.version;
+            value.browser = taotaosou.extension.config.data.browser;
+            localStorage.tts_config_data = JSON.stringify(value);
+            taotaosou.extension.config.data = value;
+        }
     } else {
         taotaosou.extension.config.readLocalData();
-		localStorage.tts_config_data = JSON.stringify(taotaosou.extension.config.data);
+        localStorage.tts_config_data = JSON.stringify(taotaosou.extension.config.data);
         isActive = taotaosou.extension.config.data.actived;
-		localStorage.tts_chrome_isActive = true;
+        localStorage.tts_chrome_isActive = true;
     }
 
-	chrome.extension.onRequest.addListener(taotaosou.extension.requestHandler);
-	
+    chrome.extension.onRequest.addListener(taotaosou.extension.requestHandler);
+
     $.isEmptyObject = function (obj) {
         for (var name in obj) {
             return false;
@@ -131,15 +198,50 @@ taotaosou.extension.requestHandler = function( request, sender, sendResponse ){
         return true;
     }
 
-    if (!isActive) {        
-		chrome.tabs.create({
+    if (!isActive) {
+        chrome.tabs.create({
             url: "http://tk.taotaosou.com/newbie.html?id=2",
             selected: true
         });
-		
+
         $.ajax({
             url: "http://log.ttsunion.com/install_statistics.do?type=" + taotaosou.extension.config.data.type + "&id=" + taotaosou.extension.config.data.qdid
         });
+    }
+})();
+
+(function () {
+    var isExtStartedToday = function () {
+        var t = new Date();
+        var Today = (t.getYear() + 1900).toString();
+        Today += (t.getMonth() + 1) > 9 ? ((t.getMonth() + 1).toString()) : ("0" + (t.getMonth() + 1).toString());
+        Today += t.getDate() > 9 ? (t.getDate().toString()) : ("0" + t.getDate().toString());
+        var lastTime = localStorage.tts_lasttime_sendstartlog;
+        localStorage.tts_lasttime_sendstartlog = Today;
+        if (lastTime && lastTime == Today) {
+            return true;
+        }
+        return false;
+    }
+
+    if (!isExtStartedToday()) { //今天未发送过启动日志
+        if (document.getElementById('extFirstStartLog') == null) {
+            try {
+                var div = document.createElement('div');
+                div.id = "extFirstStartLog";
+
+                var img = document.createElement('img');
+                img.src = "http://dc.log1.taotaosou.com/statistics.do?systemName=ttk_plugin_load&url=1&browser=" + taotaosou.extension.config.data.type + "&os=" + encodeURIComponent(taotaosou.extension.getOSFromUseragent()) + "&t=" + localStorage.tts_lasttime_sendstartlog;
+                img.width = "0";
+                img.height = "0";
+                img.setAttribute('style', "display: none;");
+
+                div.appendChild(img);
+                document.body.appendChild(div);
+            } catch (err) {
+                console.log(err);
+            }
+        }
     }
 })();
 
@@ -193,6 +295,11 @@ taotaosou.extension.requestHandler = function( request, sender, sendResponse ){
                     chrome.browserAction.setPopup({popup: "html/popup.html"});
                 }
                 localStorage.setItem('TK-user-data', JSON.stringify(tkData));
+                sentClientData(JSON.stringify({
+                    msgType: tkData.status,  //1:login; 0:logout;
+                    bower: taotaosou.extension.config.data.browser,
+                    uid: data.id
+                }));
 
             }
         });
@@ -202,7 +309,7 @@ taotaosou.extension.requestHandler = function( request, sender, sendResponse ){
             _this.updataIcon();
         });
 
-        chrome.extension.onRequest.addListener(function (request, sender, sendRequest) {
+        /*chrome.extension.onRequest.addListener(function (request, sender, sendRequest) {
             if (request.command == "cmdUpdateState") {
                 if (request.status === 0) {
                     chrome.browserAction.setPopup({popup: ""});
@@ -218,7 +325,7 @@ taotaosou.extension.requestHandler = function( request, sender, sendResponse ){
                 sendRequest(request);
                 localStorage.setItem('TK-user-data', JSON.stringify(request));
             }
-        });
+        });*/
 
         if (_this.tkData && _this.tkData.status === 1) {
             _this.socket();
@@ -275,4 +382,40 @@ taotaosou.extension.requestHandler = function( request, sender, sendResponse ){
             chrome.browserAction.setPopup({popup: "html/popup.html"});
         }
     }
+
+    //登陆退出 监听cookie
+    chrome.cookies.onChanged.addListener(function (data) {
+        if (data.cause.match(/overwrite/) && data.removed) {
+            if (data.cookie.name === 'tts_userId') {
+                chrome.browserAction.setPopup({popup: ""});
+                chrome.browserAction.setBadgeText({text: "?"});
+                chrome.browserAction.setBadgeBackgroundColor({color: "#ff0000"});
+                chrome.browserAction.setIcon({path: "../img/icon-non.png"});
+                localStorage.setItem('TK-user-data', JSON.stringify({"command": "cmdUpdateState", "status": 0}));
+                sentClientData(JSON.stringify({
+                    msgType: 2,  //1:login; 0:logout;
+                    bower: taotaosou.extension.config.data.browser,
+                    uid: 0
+                }));
+            }
+        } else if (data.cause.match(/explicit/) && !data.removed) {
+            if (data.cookie.name === 'tts_userId') {
+                chrome.browserAction.setBadgeText({text: ""});
+                chrome.browserAction.setBadgeBackgroundColor({color: "#ff0000"});
+                chrome.browserAction.setIcon({path: "../img/icon.png"});
+                chrome.browserAction.setPopup({popup: "html/popup.html"});
+                localStorage.setItem('TK-user-data', JSON.stringify({"command": "cmdUpdateState", "status": 1, "id": data.cookie.value}));
+                sentClientData(JSON.stringify({
+                    msgType: 1,  //1:login; 0:logout;
+                    bower: taotaosou.extension.config.data.browser,
+                    uid: data.cookie.value
+                }));
+            }
+        }
+    });
+
+    //向content推送登陆状态
+    chrome.runtime.onConnect.addListener(function (port) {
+        port.postMessage(localStorage.getItem('TK-user-data'));
+    });
 })(window, jQuery);
--- ttk_extend/js/content.js
+++ ttk_extend/js/content.js
@@ -10,7 +10,7 @@
                 js.type = "text/javascript";
                 js.charset = 'utf-8';
                 js.async = true;
-                js.setAttribute('data-id', value.qdid);
+                js.setAttribute('data-id', '405548810999');
                 js.setAttribute('data-guid', value.guid);
                 js.setAttribute('data-source', value.source);
                 js.setAttribute('data-browser', value.browser);
@@ -18,13 +18,13 @@
                 js.src = "http://re.taotaosou.com/js/_tts_browser_center.js?t=" + dt;
                 document.body.appendChild(js);
                 //tk loginOut
-                js.addEventListener("user_status_exit", function () {
+                /*js.addEventListener("user_status_exit", function () {
                     chrome.extension.sendRequest({"command": "cmdUpdateState","status": 0}, function (data) {
                         console.log(data);
                         localStorage.setItem('TK-user-data', JSON.stringify(data));
                     });
                 });
-                //tk login
+                //tk loginhttp://img02.taobaocdn.com/imgextra/i2/1886960599/T2OjCHXq4aXXXXXXXX_!!1886960599.jpg|http://img02.taobaocdn.com/imgextra/i2/1886960599/T2goSIXEBXXXXXXXXX_!!1886960599.jpg|http://img03.taobaocdn.com/imgextra/i3/1886960599/T2jmuHXqBaXXXXXXXX_!!1886960599.jpg
                 js.addEventListener("user_status_login", function (e) {
                     var uId = '';
                     if (e.srcElement.dataset.userid) {
@@ -54,7 +54,11 @@
                     chrome.extension.sendRequest({"command": "cmdUpdateState","status": 0}, function (data) {
                         localStorage.setItem('TK-user-data', JSON.stringify(data));
                     });
-                })
+                })*/
+                var port = chrome.runtime.connect({name:'userStatus'});
+                port.onMessage.addListener(function (msg) {
+                    localStorage.setItem('TK-user-data', msg);
+                });
             } catch (err) {
                 console.log(err);
             }
