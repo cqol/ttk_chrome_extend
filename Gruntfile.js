0b9f7e6532c7e7a421cf70db59c507f21fa32e48
chenqi <chenqi@taotaosou.com>
2015-07-31 10:07:55 +0800

    add Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,85 +1,85 @@
-module.exports = function(grunt) {
-    var today = grunt.template.today("yyyy-mm-dd HH:MM:ss");
+module.exports = function (grunt) {
+	var today = grunt.template.today("yyyy-mm-dd HH:MM:ss");
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-        jshint:{
-            options: {
-                jshintrc: '.jshintrc'
-            },
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
 
-            ttk: {
-                files: {
-                    src: ['ttk_extend/js/*.js', 'ttk_extend/js/login/*.js', '!ttk_extend/js/lib/*.js']
-                }
-            }
-        },
+			ttk: {
+				files: {
+					src: ['ttk_extend/js/*.js', 'ttk_extend/js/login/*.js', '!ttk_extend/js/lib/*.js']
+				}
+			}
+		},
 
-        uglify: {
-            options: {
-                banner: '/* @date:' + today + ' */\n'
-            },
-            ttk: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'dist/ttk_chrome_extend/js/login/',
-                        src: '*.js',
-                        dest: 'dist/ttk_chrome_extend/js/login/'
-                    },
-                    {
-                        expand: true,
-                        cwd: 'dist/ttk_chrome_extend/js/',
-                        src: '*.js',
-                        dest: 'dist/ttk_chrome_extend/js/'
-                    }
-                ]
-            }
-        },
-        cssmin: {
-            options: {
-                banner: '/* @date:' + today + ' */'
-            },
-            minify: {
-                expand: true,
-                cwd: 'dist/ttk_chrome_extend/css/',
-                src: ['*.css'],
-                dest: 'dist/ttk_chrome_extend/css/',
-                ext: '.css'
-            }
-        },
-        copy: {
-            ttk: {
-                files: [
+		uglify: {
+			options: {
+				banner: '/* @date:' + today + ' */\n'
+			},
+			ttk: {
+				files: [
+					{
+						expand: true,
+						cwd: 'dist/ttk_chrome_extend/js/login/',
+						src: '*.js',
+						dest: 'dist/ttk_chrome_extend/js/login/'
+					},
+					{
+						expand: true,
+						cwd: 'dist/ttk_chrome_extend/js/',
+						src: '*.js',
+						dest: 'dist/ttk_chrome_extend/js/'
+					}
+				]
+			}
+		},
+		cssmin: {
+			options: {
+				banner: '/* @date:' + today + ' */'
+			},
+			minify: {
+				expand: true,
+				cwd: 'dist/ttk_chrome_extend/css/',
+				src: ['*.css'],
+				dest: 'dist/ttk_chrome_extend/css/',
+				ext: '.css'
+			}
+		},
+		copy: {
+			ttk: {
+				files: [
 
-                    {expand: true, cwd: 'ttk_extend/', src: '**/*', dest: 'dist/ttk_chrome_extend/'}
-                ]
-            }
-        }
-    });
+					{expand: true, cwd: 'ttk_extend/', src: '**/*', dest: 'dist/ttk_chrome_extend/'}
+				]
+			}
+		}
+	});
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
 
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    //淘同款入口
-    grunt.registerTask('ttk', 'ttk extend', function() {
-        //grunt.task.run('jshint:ttk');
+	//淘同款入口
+	grunt.registerTask('ttk', 'ttk extend', function () {
+		//grunt.task.run('jshint:ttk');
 
-        grunt.task.run('copy:ttk');
-        grunt.task.run('uglify:ttk');
-        grunt.task.run('cssmin');
-    });
+		grunt.task.run('copy:ttk');
+		grunt.task.run('uglify:ttk');
+		grunt.task.run('cssmin');
+	});
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+	// Default task(s).
+	grunt.registerTask('default', ['uglify']);
 
 };
